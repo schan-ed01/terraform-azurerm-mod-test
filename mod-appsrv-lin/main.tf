b07fd0162bab55ffc0b1e13db98ac16b599a6471
Suet.Chan <schan.ed01@gmail.com>
2022-08-17 15:19:12 -0400

    testing

---
+++ mod-appsrv-lin/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = "= 3.8.0"
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+variable "rg_name" {
+  type        = string
+  description = "rg name"
+  default = "lc_test_rg_01"
+}
+
+variable "location" {
+  type        = string
+  description = "rg name"
+  default = "eastus2"
+}
+
+
+resource "azurerm_resource_group" "this" {
+    name = var.rg_name
+    location = var.location
+    tags = {purpose = "Testing"}
+}
